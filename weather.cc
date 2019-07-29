45cd327415d55d33ca1cf309fc525a7fb13b6fff
Drew Vlasnik <314dsv@gmail.com>
2019-07-28 20:51:34 -0400

    Added weather

---
+++ weather.cc
@@ -0,0 +1,18 @@
+//	Weather Script:
+//		This program will interact with
+//	    the Dark Sky API via curl to
+//      retrieve local weather information
+//
+//		It requires the secret API key
+//		and latitude and longitude
+//
+//      Returns weather information in JSON format
+
+#include <iostream>
+
+std::string API_KEY = "ccbd29d37e864cd514cd6bb057c7dd8f";
+
+int main()
+{
+
+}
