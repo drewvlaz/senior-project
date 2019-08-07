700267235442008450e7941eaca7153c484a9d5f
drewvlaz <314dsv@gmail.com>
2019-08-07 01:00:23 +0000

    Added enum

--- main.cc
+++ main.cc
@@ -1,12 +1,34 @@
 #include <iostream>
 #include "MultinomialNB.h"
 
+enum Classification {
+    grades,
+    greeting,
+    jokes,
+    search,
+    weather
+};
+
+void ExecuteUserRequest(Classification classification) {
+    switch (classification) {
+        case greeting:
+            std::cout << "Hello, my name is TBD" << "\n";
+            break;
+        
+        default:
+            break;
+        }
+}
+
 int main() {
     MultinomialNB model;
 
     model.ReadInTrainingData();
     model.PrepareData();
     model.Train();
+
+    enum Classification test = greeting;
+    ExecuteUserRequest(test);
 
     std::string sentence = "hey hows it going, my name is drew";
     std::string prediction = model.Classify(sentence);
